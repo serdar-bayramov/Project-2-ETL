4e89854d815c65603b932238952d85ade8e5fc0b
Serdar Bayramov <85737622+serdar-bayramov@users.noreply.github.com>
2021-09-17 12:53:19 +0100

    final data

--- sql.sql
+++ sql.sql
@@ -1,5 +1,8 @@
 CREATE TABLE vaccination (
 Total_Vaccinations_UK TEXT, 
 Total_Vaccinations_Canada TEXT,
-Total_Vaccinations_US TEXT);
+Total_Vaccinations_US TEXT,
+Total_Vaccinations_Italy TEXT);
 SELECT * FROM vaccination;
+
+
